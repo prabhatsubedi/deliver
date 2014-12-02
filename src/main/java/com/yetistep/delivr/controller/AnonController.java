eb1851c005adcbe92fc0d9552ff18bad1a8bfcd6
surendraJ <surendra@yetistep.com>
2014-12-02 14:40:28 +0545

    amazon jackson conflict fixed

---
+++ src/main/java/com/yetistep/delivr/controller/AnonController.java
@@ -0,0 +1,13 @@
+package com.yetistep.delivr.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: surendraJ
+ * Date: 12/2/14
+ * Time: 2:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AnonController {
+    /* Controller For All User */
+
+}
---
+++ src/main/java/com/yetistep/delivr/controller/ClientController.java
@@ -0,0 +1,13 @@
+package com.yetistep.delivr.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: surendraJ
+ * Date: 12/2/14
+ * Time: 2:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClientController {
+    /* Controller for Mobile API */
+
+}

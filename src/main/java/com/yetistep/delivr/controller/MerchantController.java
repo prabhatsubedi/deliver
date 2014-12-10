8c7e8a75fa2265dd9a6525bc2bc09c24ad86f55b
Sagar Sapkota <sagar@yetistep.com>
2014-12-10 12:31:44 +0545

    clean commented code

--- src/main/java/com/yetistep/delivr/controller/MerchantController.java
+++ src/main/java/com/yetistep/delivr/controller/MerchantController.java
@@ -36,36 +36,10 @@ public class MerchantController {
     @ResponseBody
     public ResponseEntity<ServiceResponse> saveStore(@RequestHeader HttpHeaders headers, @RequestBody List<StoreEntity> stores) {
         try {
-            /*HeaderDto headerDto = new HeaderDto();
-            GeneralUtil.fillHeaderCredential(headers, headerDto);*/
-            /*
-           private Integer id;
-            private StoresBrandsEntity storesBrand;
-            private Set<CategoryEntity> category = new HashSet<CategoryEntity>();
-            private Set<ItemsStoreEntity> itemsStore = new HashSet<ItemsStoreEntity>();
-            private Set<OrderEntity> order = new HashSet<OrderEntity>();
-            private String name;
-            private String street;
-            private String locality;
-            private String city;
-            private String country;
-            private String contact_no;
-            private BigDecimal latitude;
-            private BigDecimal longitude;
-            private Timestamp created_date;
-            private String return_policy;
-            private Integer delivery_fee;
-            private String promo_code;
-            private BigDecimal vat;
-            private BigDecimal service_charge;
-        * */
-
             merchantService.saveStore(stores, headers);
 
-            ServiceResponse serviceResponse = new ServiceResponse("Store has been saved successfully");
-//            HttpHeaders httpHeaders = new HttpHeaders();
-//            httpHeaders.add("deliveryBoyId", deliveryBoy.getUser().getId() + "");
-//            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+            ServiceResponse serviceResponse = new ServiceResponse("Store(Stores) has been saved successfully");
+
             return new ResponseEntity<ServiceResponse>(serviceResponse, HttpStatus.CREATED);
 
         } catch (Exception e) {

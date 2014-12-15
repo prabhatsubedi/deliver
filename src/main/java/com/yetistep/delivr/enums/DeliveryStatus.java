9d9044ea2f4261c63f1cbaa1733a12fdef4316db
abhiraj <abhiraj@yetistep.com>
2014-12-15 11:08:25 +0545

    Delivery status and JOB status enum added

---
+++ src/main/java/com/yetistep/delivr/enums/DeliveryStatus.java
@@ -0,0 +1,15 @@
+package com.yetistep.delivr.enums;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Chandra Prakash Panday
+ * Date: 12/12/14
+ * Time: 5:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum DeliveryStatus {
+    SUCCESSFUL,
+    UNSUCCESSFUL,
+    CANCELLED,
+    ASSIGNED_TO_OTHER;
+}
---
+++ src/main/java/com/yetistep/delivr/enums/JobOrderStatus.java
@@ -0,0 +1,17 @@
+package com.yetistep.delivr.enums;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Chandra Prakash Panday
+ * Date: 12/12/14
+ * Time: 4:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum JobOrderStatus {
+    ORDER_PLACED, /* when checkout is successful */
+    ORDER_ACCEPTED, /* when delivery boy clicks ACCEPT */
+    IN_ROUTE_TO_PICK_UP, /* when delivery boy clicks START JOB */
+    AT_STORE, /* when delivery boy clicks AT STORE */
+    IN_ROUTE_TO_DELIVERY, /* when delivery boy clicks IN ROUTE TO DELIVERY */
+    DELIVERED; /*when delivery boy enter the receive CODE */
+}

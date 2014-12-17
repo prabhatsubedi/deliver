847afd60c34e9f0ac8b21c32655d27d3cd67fdd8
surendraJ <surendra@yetistep.com>
2014-12-17 12:32:20 +0545

    algorithm service added

---
+++ src/main/java/com/yetistep/delivr/service/impl/SystemAlgorithmServiceImpl.java
@@ -0,0 +1,18 @@
+package com.yetistep.delivr.service.impl;
+
+import com.yetistep.delivr.service.inf.SystemAlgorithmService;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: surendraJ
+ * Date: 12/17/14
+ * Time: 12:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SystemAlgorithmServiceImpl implements SystemAlgorithmService{
+
+    @Override
+    public void calculateCourierEarning() throws Exception {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ src/main/java/com/yetistep/delivr/service/inf/SystemAlgorithmService.java
@@ -0,0 +1,12 @@
+package com.yetistep.delivr.service.inf;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: surendraJ
+ * Date: 12/17/14
+ * Time: 12:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface SystemAlgorithmService {
+    public void calculateCourierEarning() throws Exception;
+}

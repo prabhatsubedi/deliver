b1c179114643c616ac87b6836947e98db1054922
surendraJ <surendra@yetistep.com>
2014-12-19 10:54:25 +0545

    Courier Transaction Dao Implemented

---
+++ src/main/java/com/yetistep/delivr/dao/impl/CourierTransactionDaoServiceImpl.java
@@ -0,0 +1,53 @@
+package com.yetistep.delivr.dao.impl;
+
+import com.yetistep.delivr.dao.inf.CourierTransactionDaoService;
+import com.yetistep.delivr.model.CourierTransactionEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: surendraJ
+ * Date: 12/19/14
+ * Time: 10:48 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CourierTransactionDaoServiceImpl implements CourierTransactionDaoService{
+    @Autowired
+    SessionFactory sessionFactory;
+
+    @Override
+    public CourierTransactionEntity find(Integer id) throws Exception {
+        return (CourierTransactionEntity) getCurrentSession().get(CourierTransactionEntity.class, id);
+    }
+
+    @Override
+    public List<CourierTransactionEntity> findAll() throws Exception {
+        return (List<CourierTransactionEntity>) getCurrentSession().createCriteria(CourierTransactionEntity.class).list();
+    }
+
+    @Override
+    public Boolean save(CourierTransactionEntity value) throws Exception {
+        getCurrentSession().save(value);
+        return true;
+    }
+
+    @Override
+    public Boolean update(CourierTransactionEntity value) throws Exception {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Boolean delete(CourierTransactionEntity value) throws Exception {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Session getCurrentSession() throws Exception {
+        Session session = sessionFactory.getCurrentSession();
+        return session;
+    }
+}
---
+++ src/main/java/com/yetistep/delivr/dao/inf/CourierTransactionDaoService.java
@@ -0,0 +1,14 @@
+package com.yetistep.delivr.dao.inf;
+
+import com.yetistep.delivr.abs.GenericDaoService;
+import com.yetistep.delivr.model.CourierTransactionEntity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: surendraJ
+ * Date: 12/19/14
+ * Time: 10:47 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface CourierTransactionDaoService extends GenericDaoService<Integer, CourierTransactionEntity>{
+}

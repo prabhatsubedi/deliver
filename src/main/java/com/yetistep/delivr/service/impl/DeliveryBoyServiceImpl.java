7189b6c9a56ade703313e84bd618a91480c9ea31
abhiraj <abhiraj@yetistep.com>
2015-01-22 17:42:12 +0545

    Developer's Comment added while creating order, and order_verification_code changed to String

--- src/main/java/com/yetistep/delivr/service/impl/DeliveryBoyServiceImpl.java
+++ src/main/java/com/yetistep/delivr/service/impl/DeliveryBoyServiceImpl.java
@@ -373,6 +373,7 @@ public class DeliveryBoyServiceImpl implements DeliveryBoyService {
         if(!order.getDeliveryBoy().getId().equals(deliveryBoyId)){
             throw new YSException("ORD003");
         }
+        //TODO check order id as well
         if(!orderEntity.getOrderVerificationCode().equals(order.getOrderVerificationCode())){
             throw new YSException("ORD004");
         }

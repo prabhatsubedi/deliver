d91e84cdfa8327210d38b35fa51d556f9b91526f
Sagar Sapkota <sagar@yetistep.com>
2015-03-18 15:51:11 +0545

    schedule time

--- src/main/java/com/yetistep/delivr/schedular/TaskSchedule.java
+++ src/main/java/com/yetistep/delivr/schedular/TaskSchedule.java
@@ -47,7 +47,7 @@ public class TaskSchedule {
 
     }
 
-    @Scheduled(cron="0 22 17 * * TUE")
+    @Scheduled(cron="0 0 0 * * WED")
     public void generateInvoice() throws Exception{
         log.info("Generating invoice:");
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");

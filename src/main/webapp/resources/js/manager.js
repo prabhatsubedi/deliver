e616a2162d9662ca92135c1e2c84eb0bb359165f
Lunek <lunek@yetistep.com>
2015-04-13 15:32:52 +0545

    - minor changes

--- src/main/webapp/resources/js/manager.js
+++ src/main/webapp/resources/js/manager.js
@@ -706,9 +706,9 @@ if (typeof(Manager) == "undefined") var Manager = {};
                 Main.request('/accountant/get_dboy', {}, callback, headers);
             }, 300);
         });
-    }
+    };
 
-    Manager.getCourierBoyAccount = function () {
+    Manager.getTransactions = function () {
 
         var dataFilter = function (data, type) {
             if (!data.success) {
@@ -756,6 +756,10 @@ if (typeof(Manager) == "undefined") var Manager = {};
         dataFilter.headers = headers;
 
         Main.createDataTable("#detail_account_table", dataFilter);
+
+    };
+
+    Manager.getCourierBoyAccount = function () {
 
         var callback = function (status, data) {
             if (!data.success) {
@@ -939,6 +943,7 @@ if (typeof(Manager) == "undefined") var Manager = {};
     }
 
     Manager.fillDboyAccount = function(courierStaff){
+        Manager.getTransactions();
         if(typeof courierStaff.previousDue != "undefined"){
             $(".due_amount").text(parseFloat(courierStaff.previousDue).toFixed(2));
             $("#due_amount_val").val(parseFloat(courierStaff.previousDue).toFixed(2));

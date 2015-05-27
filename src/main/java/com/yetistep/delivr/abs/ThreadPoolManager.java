73de8de2deb481cef05d4a8f59184217a222db49
abhiraj <abhiraj@yetistep.com>
2015-05-27 12:08:16 +0545

    Formatting done

--- src/main/java/com/yetistep/delivr/abs/ThreadPoolManager.java
+++ src/main/java/com/yetistep/delivr/abs/ThreadPoolManager.java
@@ -14,32 +14,32 @@ import java.util.concurrent.TimeUnit;
  * To change this template use File | Settings | File Templates.
  */
 public class ThreadPoolManager {
-    private static final Logger log     =   Logger.getLogger(ThreadPoolManager.class);
+    private static final Logger log = Logger.getLogger(ThreadPoolManager.class);
     private static ExecutorService pool;
 
-    public static void init(){
-        if(pool == null)
+    public static void init() {
+        if (pool == null)
             pool = Executors.newFixedThreadPool(5);//fixed pool size 5
 
     }
 
-    public static void runAsynchJob(Runnable job){
-       init();
-       pool.execute(job);
+    public static void runAsynchJob(Runnable job) {
+        init();
+        pool.execute(job);
     }
 
     public static void shutdownAndAwaitTermination() {
-        log.info("Shutting signal obtained and clearing the pool: "+pool);
+        log.info("Shutting signal obtained and clearing the pool: " + pool);
         pool.shutdown(); // Disable new tasks from being submitted
         try {
             if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {// Wait a while for existing tasks to terminate
                 pool.shutdownNow(); // Cancel currently executing tasks
 
                 if (!pool.awaitTermination(60, TimeUnit.SECONDS))// Wait a while for tasks to respond to being cancelled
-                    log.error("Pool did not termiate");
+                    log.error("Pool did not terminate");
             }
         } catch (InterruptedException ie) {
-            log.error("Error occurred while clearing the pool",ie);
+            log.error("Error occurred while clearing the pool", ie);
             pool.shutdownNow();
             Thread.currentThread().interrupt();// Preserve interrupt status
         } catch (Exception e) {

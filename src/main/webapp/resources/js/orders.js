67b693de2f612a4e331251918ad2d578a3b981b8
Lunek <lunek@yetistep.com>
2015-06-04 11:27:03 +0545

    - minor changes

--- src/main/webapp/resources/js/orders.js
+++ src/main/webapp/resources/js/orders.js
@@ -672,7 +672,7 @@ Order.getOrdersItems = function(){
             { "name": "itemTotal" }
         ];
         dataFilter.headers = {id:$(e.relatedTarget).data('id')};
-        dataFilter.pageSize = undefined;
+        dataFilter.pageSize = 0;
         Main.createDataTable("#orders_items_table", dataFilter);
 
     });

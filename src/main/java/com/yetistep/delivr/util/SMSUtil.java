bbdfe3e73caf2b0a6de825cc3dac06b4c15f1651
Sagar Sapkota <sagar@yetistep.com>
2015-06-11 15:04:56 +0545

    version 2 removed from master

--- src/main/java/com/yetistep/delivr/util/SMSUtil.java
+++ src/main/java/com/yetistep/delivr/util/SMSUtil.java
@@ -1,18 +1,5 @@
 package com.yetistep.delivr.util;
 
-import com.twilio.sdk.TwilioRestClient;
-import com.twilio.sdk.TwilioRestException;
-import com.twilio.sdk.resource.factory.MessageFactory;
-import com.twilio.sdk.resource.instance.Message;
-import com.twilio.sdk.resource.list.UsageRecordList;
-import com.yetistep.delivr.enums.PreferenceType;
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.log4j.Logger;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created with IntelliJ IDEA.
  * User: Sagar
